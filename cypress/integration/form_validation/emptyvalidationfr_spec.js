a76e28da58d20703b9b8e46d356115e273716d75
David Olarte <david.olarte@digitalmediafactory.ch>
2021-09-25 12:04:15 +0800

    Check FR page and execute validation

---
+++ cypress/integration/form_validation/emptyvalidationfr_spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Visit Page in FR and Form Validation', function(){
+    it('Open Page in FR', function(){
+     //Visit Page in CH
+     cy.visit('https://int.dermaplast.viu.ch/fr/')//
+ 
+     //Clicking the Anmelden / User icon
+     cy.wait(5000).get('.nav-item__user > .nav-link').click()
+ 
+     //Clicking Registrieren button
+      cy.wait(3000).get('.user-dropdown__actions > .button-2').click()
+ 
+     // Form Validation
+     // Condition - Leave all fields empty and click Registrieren button.
+     // Expected Results - Display all warnings for all mandatory fields.
+     cy.wait(2000).get('#register-button').click()
+ 
+     //Expected Result
+     cy.log('Expected Result :  Display all warnings for all mandatory fields.')
+ 
+    });
+ 
+ })
